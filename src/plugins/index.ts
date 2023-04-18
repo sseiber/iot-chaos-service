3dede519a2e49ea87194bdc7854f3337179bd6c9
Scott Seiber <Scott.Seiber@microsoft.com>
2023-04-18 12:01:04 -0700

    WIP

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,9 +1,11 @@
 import { AuthPlugin } from './auth';
 import { LoopBoxProxyPlugin } from './loopBoxProxy';
 import { ErrorRedirectPlugin } from './errorRedirect';
+import { RequestTimePlugin } from './requestTime';
 
 export default [
     AuthPlugin,
     LoopBoxProxyPlugin,
-    ErrorRedirectPlugin
+    ErrorRedirectPlugin,
+    RequestTimePlugin
 ];
